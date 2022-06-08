76cf0e8db3ea494447f606b7485fad6a1bf7cee2
Shin-sibainu <shincode0712@gmail.com>
2022-06-08 20:09:50 +0900

    done

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,18 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#83D483",
+        secondary: "6C6C6C",
+      },
+      backgroundImage: {
+        "tutorial-bg": "url('../src/img/main-bg.jpg')",
+      },
+    },
+    fontFamily: {
+      poppins: ["Poppins", "sans-serif"],
+    },
   },
   plugins: [],
 };
